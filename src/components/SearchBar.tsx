08c8a37f98156fee0bd087f9e7ef57d3ed0e5585
Sam Chou <tangerinekey381@gmail.com>
2024-07-16 17:42:13 +0800

    updated searchbar length

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -29,30 +29,15 @@ export default function SearchBar({
           value={query}
           onChange={e => setQuery(e.target.value)}
           placeholder={placeholder}
-          className="p-2 px-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 searchbar-bg"
+          className="w-128 p-2 px-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 searchbar-bg"
         />
-        <div className=" absolute pl-3 inset-y-0 flex items-center pointer-events-none">
+        <div className="absolute pl-3 inset-y-0 flex items-center pointer-events-none">
           <MagnifyingGlassIcon className="w-5 h-5 text-gray-400" />
         </div>
         {/* <div className="">
           <XCircleIcon className="w-6 h-6 text-gray-400" />
         </div> */}
       </div>
-      {/* {query && (
-        <button
-          type="button"
-          onClick={() => setQuery("")}
-          className="absolute right-0 p-2 bg-red-500 text-white rounded-r-lg hover:bg-red-600 focus:outline-none"
-        >
-          Clear
-        </button>
-      )} */}
-      {/* <button
-        type="submit"
-        className="ml-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
-      >
-        {buttonText}
-      </button> */}
     </form>
   );
 }
