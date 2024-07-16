7fc59723f975968995d30a0f3b6fe2153ec7b3b2
Sam Chou <tangerinekey381@gmail.com>
2024-07-16 16:55:42 +0800

    webpack changed test

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-};
-
-export default nextConfig;
-
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+
+//     return config;
+//   },
+// };
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+export default nextConfig;
 
 // const withPlugins = require('next-compose-plugins');
 // const withSvgr = require('next-svgr');
