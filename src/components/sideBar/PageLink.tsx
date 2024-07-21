4e389f4d19291f068599ebd5e579f3c8edeba58d
fuzzykala <tangerinekey381@gmail.com>
2024-07-21 17:42:41 +0800

    refactor: Update PageLink component to use PageLinkItem for rendering individual links

--- src/components/sideBar/PageLink.tsx
+++ src/components/sideBar/PageLink.tsx
@@ -1,37 +1,27 @@
 import { useRouter, usePathname } from "next/navigation";
 import styles from "@/components/sideBar/pageLink.module.css";
+import PageLinkItem from "./PageLinkItem";
+
+interface Link {
+  href: string;
+  label: string;
+}
+const links: Link[] = [
+  { href: "/", label: "Home" },
+  { href: "/popular", label: "Popular" },
+  { href: "/following", label: "Following" },
+];
 
 export default function PageLink() {
   const router = useRouter();
   const currentPage = usePathname();
   console.log("Current page:", currentPage);
-  const handleLinkStyle = (page: string) => {
-    return currentPage === page ? styles.activeLink : styles.inactiveLink;
-  };
 
   return (
-    <div>
-      <div
-        id="homeLink"
-        className={`${styles.pageLink} ${handleLinkStyle("/")}`}
-        onClick={() => router.push("/")}
-      >
-        <p className="py-1 px-2">Home</p>
-      </div>
-      <div
-        id="popularLink"
-        className={`${styles.pageLink} ${handleLinkStyle("/popular")}`}
-        onClick={() => router.push("/popular")}
-      >
-        <p className="py-1 px-2">Popular</p>
-      </div>
-      <div
-        id="followingLink"
-        className={`${styles.pageLink} ${handleLinkStyle("/following")}`}
-        onClick={() => router.push("/following")}
-      >
-        <p className="py-1 px-2">Following</p>
-      </div>
+    <div id="pagesContainer">
+      {links.map((link) => (
+        <PageLinkItem key={link.href} currentPage={currentPage} link={link} />
+      ))}
     </div>
   );
 }
---
+++ src/components/sideBar/PageLinkItem.tsx
@@ -0,0 +1,30 @@
+import styles from "@/components/sideBar/pageLink.module.css";
+import { useRouter } from "next/navigation";
+
+interface Link {
+  href: string;
+  label: string;
+}
+interface PageLinkItemProps {
+  currentPage: string;
+  link: Link;
+}
+
+export default function PageLinkItem({ currentPage, link }: PageLinkItemProps) {
+  const router = useRouter();
+  const handleLinkStyle = (page: string): string => {
+    return currentPage === page ? styles.activeLink : styles.inactiveLink;
+  };
+
+  return (
+    <div
+      className={`${styles.pageLink} ${handleLinkStyle(link.href)}`}
+      onClick={() => router.push(link.href)}
+      role="button"
+      aria-label={`Navigate to ${link.label}`}
+      tabIndex={0}
+    >
+      <p className="py-1 px-2">{link.label}</p>
+    </div>
+  );
+}
